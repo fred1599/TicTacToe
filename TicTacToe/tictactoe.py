382662edfb239520c24f2bc4c36a5722f2746c44
fred1599 <fred1599@hotmail.com>
2019-02-14 00:52:29 +0100

    TicTacToe Game

---
+++ TicTacToe/tictactoe.py
@@ -0,0 +1,98 @@
+from dataclasses import dataclass
+
+from tkinter import messagebox
+
+from utils import check
+from utils import check_game
+from utils import grid
+
+import tkinter as tk
+import copy
+
+@dataclass
+class Game(tk.Tk):
+    notfinish: bool = True
+    again: bool = False
+
+    def __post_init__(self):
+        super().__init__()
+
+        self.size = len(grid)
+        self.grid = grid
+
+        self.w, self.h = 320, 320
+        self.space = 10
+        self.square = int((self.w - 2 * self.space) // self.size)
+
+        self.canvas = tk.Canvas(self, width=self.w, height=self.h)
+        self.canvas.bind("<Button-1>", self.draw_canvas)
+        self.canvas.pack()
+
+        self.player = 1
+        self.colors = ('red', 'blue')
+
+        self.init()
+
+
+        self.mainloop()
+
+    def update(self):
+        Game.notfinish = check_game(self.grid)
+        if not Game.notfinish:
+            self.canvas.update_idletasks()
+        else:
+            self.display(f"Le gagnant est: {self.player}", question=False)
+            Game.again = self.display("Voulez-vous rejouer ?", question=True)
+            if Game.again:
+                self.init()
+                self.player = self.player % 2 + 1 # another player play first
+            else:
+                self.quit()
+
+    def init(self):
+        Game.notfinish = True
+        self.grid = [[0 for _ in range(self.size)] for _ in range(self.size)]
+        for y in range(
+                self.space,
+                self.square * self.size,
+                self.square
+            ):
+            for x in range(
+                self.space,
+                self.square * self.size,
+                self.square
+            ):
+                
+                self.canvas.create_rectangle(
+                    x, y, 
+                    x + self.square,
+                    y + self.square,
+                    fill="white"
+                )
+
+    def draw_canvas(self, event):
+        x, y = event.x, event.y
+        case = self.canvas.find_closest(x, y) 
+        x, y, x1, y1 = self.canvas.coords(case)
+        column = int((x1 - self.square) // self.square)
+        line = int((y1 - self.square) // self.square)
+        if check(self.grid, line, column, self.player):
+            self.draw(self.player, x+5, y+5, x1-5, y1-5) # 5 is space between square and oval
+            self.update()
+
+    def draw(self, figure, *coords):
+        color = self.colors[figure-1]
+        self.canvas.create_oval(*coords, fill=color)
+
+
+    def display(self, message, question=True):
+        if question:
+            res = messagebox.askquestion('question', message)
+            if res == 'yes': return True
+        else:
+            messagebox.showinfo('WINNER', message)
+        return False
+
+
+
+Game()
